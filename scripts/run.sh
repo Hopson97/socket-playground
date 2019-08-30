91569c79aba60e339391d86e59782a6ff1e85050
Hopson97 <mhopson@hotmail.co.uk>
2019-08-30 12:21:36 +0100

    Make build scripts for server/client

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,17 @@
 
 if [ "$1" = "release" ]
 then
-    ./bin/release/chat 
+    if [ "$2" = "server" ]
+    then
+        ./bin/release/chat server _
+    else
+        ./bin/release/chat client x
+    fi
 else
-    ./bin/debug/chat 
+    if [ "$2" = "server" ]
+    then
+        ./bin/debug/chat server _
+    else
+        ./bin/debug/chat client x
+    fi
 fi
