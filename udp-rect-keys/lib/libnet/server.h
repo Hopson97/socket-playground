fe8c467ad514dc625afb8f138e1616a15cddb7e0
Hopson97 <mhopson@hotmail.co.uk>
2019-09-01 22:43:40 +0100

    temp

--- udp-rect-keys/lib/libnet/server.h
+++ udp-rect-keys/lib/libnet/server.h
@@ -14,3 +14,41 @@ namespace net {
             sf::Clock m_interalClock;
     };
 }
+
+//#include<functional>
+//#include <array>
+//#include <cstdio>
+//
+//template<typename E>
+//struct CommandThing {
+//    CommandThing() {}
+//
+//    void give(E command, std::function<void()> f) {
+//        cmds[static_cast<std::size_t>(command)] = f;
+//    }
+//
+//    void invoke(E command) {
+//        cmds[static_cast<std::size_t>(command)]();
+//    }
+//
+//    std::array<std::function<void()>, static_cast<std::size_t>(E::SIZE)> cmds;    
+//};
+//
+//enum class Test {
+//    HandleConnect,
+//
+//    SIZE
+//};
+//
+//int main() {
+//    CommandThing<Test> test;
+//
+//    test.give(Test::HandleConnect, []() {
+//        int x = 5;
+//        int y =10;
+//        printf("%d", x + y);
+//    });
+//
+//    test.invoke(Test::HandleConnect);
+//
+//}
